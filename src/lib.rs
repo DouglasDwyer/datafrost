135cdb04a20e9adabb43c6dbcfefb0630c6a8041
Douglas Dwyer <dougmdwyer@hotmail.com>
2024-03-06 20:53:53 -0500

    Update API to allow for multiple derived descriptors, uninitialized object creation, and command buffers

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 #![allow(warnings)]
+#![deny(missing_docs)]
+
+//! Crate docs
 
 pub use mutability_marker::*;
 use private::*;
 use std::marker::*;
 use std::ops::*;
-use std::process::CommandArgs;
+use std::sync::*;
 use task_pool::*;
 
 /// Denotes a general class of formats, which all share similar data.
@@ -21,37 +24,60 @@ pub trait Kind: 'static + Send + Sync {
 pub trait Format: 'static + Send + Sync + Sized {
     /// The kind of data that this format represents.
     type Kind: Kind;
+
+    /// Allocates a new object of this format for the provided descriptor with
+    /// unspecified contents.
+    fn allocate(descriptor: &<Self::Kind as Kind>::FormatDescriptor) -> Self;
 }
 
-/// A format which can act as a maintained "copy" of a parent
+/// Allows a format to act as a maintained "copy" of a parent
 /// by synchronizing its contents with those of the parent.
-pub trait DerivedFormat<F: Format>: Format<Kind = F::Kind> {
-    /// Creates a new instance of this format to track the given parent.
-    fn create(descriptor: &<F::Kind as Kind>::FormatDescriptor, parent: &F) -> Self;
+pub trait DerivedDescriptor<F: Format> {
+    /// The format that should be created for this descriptor.
+    type Format: Format<Kind = F::Kind>;
 
-    /// Updates this format based upon the portions of the parent that have changed.
-    fn update(&mut self, parent: &F, usages: &[<Self::Kind as Kind>::UsageDescriptor]);
-}
-
-/// Allows for instantiating a format.
-pub trait FormatDescriptor: 'static + Send + Sync + Sized {
-    /// The format of data that should be created.
-    type Format: Format;
-
-    /// Creates the specified instance of this format.
-    fn create(self) -> Self::Format;
-
-    /// Returns a structure holding properties common to the kind of this format.
-    fn descriptor(&self) -> <<Self::Format as Format>::Kind as Kind>::FormatDescriptor;
-
-    /// Gets the label that should be associated with this object.
-    fn label(&self) -> Option<&'static str>;
+    /// Updates the given data based upon the portions of the parent that have changed.
+    fn update(&self, data: &mut Self::Format, parent: &F, usages: &[<F::Kind as Kind>::UsageDescriptor]);
 }
 
 /// A type-erased instance of [`View`] that is used to specify
-/// handle usage in a [`CommandDescriptor`].
+/// data usage in a [`CommandDescriptor`].
 pub trait ViewEntry: 'static + Send + Sync + Sealed {
     // todo: stuff to get resource ID
+}
+
+/// Determines how a new data object will be allocated.
+pub struct AllocationDescriptor<'a, F: Format> {
+    /// The descriptor describing the layout of the data.
+    pub descriptor: <F::Kind as Kind>::FormatDescriptor,
+    /// An optional label to associate with the object.
+    pub label: Option<&'static str>,
+    /// Derived formats that should be set up to track the parent format.
+    pub derived_formats: &'a [&'a Derived<F>]
+}
+
+/// Records a list of operations that should be executed on formatted data.
+pub struct CommandBuffer {
+
+}
+
+impl CommandBuffer {
+    /// Creates a new command buffer with the provided descriptor.
+    pub fn new(descriptor: CommandBufferDescriptor) -> Self {
+        todo!()
+    }
+
+    /// Schedules a command to execute on format data.
+    pub fn schedule(&self, descriptor: CommandDescriptor<impl Send + Sync + FnOnce(CommandContext)>) {
+        todo!()
+    }
+}
+
+/// Specifies the parameters of a new [`CommandBuffer`].
+#[derive(Copy, Clone, Debug, Default)]
+pub struct CommandBufferDescriptor {
+    /// An optional label to associate with the object.
+    pub label: Option<&'static str>
 }
 
 /// Allows for interacting with format data during command execution.
@@ -95,19 +121,66 @@ impl<'a, C: 'static + Send + Sync + FnOnce(CommandContext)> std::fmt::Debug for 
     }
 }
 
+/// Determines how a [`DataFrostContext`] should behave.
+#[derive(Copy, Clone, Debug, Default)]
+pub struct ContextDescriptor {
+    /// An optional label to associate with the object.
+    pub label: Option<&'static str>
+}
+
+impl<F: Format> std::fmt::Debug for Derived<F> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+/// References an object of a specific [`Kind`]. The object is backed
+/// by a single primary format and some number of derived formats.
+pub struct Data<K: Kind> {
+    marker: PhantomData<K>
+}
+
+impl<K: Kind> Data<K> {
+    /// Creates a view of this data of the given format and mutability.
+    /// The format must either be the primary format or one of the derived
+    /// formats specified during object creation. If this is a mutable view,
+    /// then the format must be the primary format. Using views with invalid
+    /// mutability or formats will lead to panics when [`DataFrostContext::submit`]
+    /// is called.
+    pub fn view<M: Mutability, F: Format>(&self) -> View<M, F> {
+        todo!("Where to put the check for getting data?")
+    }
+}
+
+impl<K: Kind> Clone for Data<K> {
+    fn clone(&self) -> Self {
+        todo!()
+    }
+}
+
+impl<K: Kind> std::fmt::Debug for Data<K> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+/// Manages a collection of formatted data, coordinates the execution of commands
+/// against the data, and automatically updates derived formats using the results
+/// of those commands.
 #[derive(Clone)]
-pub struct FrostContext {
+pub struct DataFrostContext {
     
 }
 
-impl FrostContext {
-    pub fn new() -> Self {
+impl DataFrostContext {
+    /// Allocates a new, empty context.
+    pub fn new(descriptor: ContextDescriptor) -> Self {
         todo!()
     }
 
     /// Creates a new object from the provided descriptor. Objects are also created for each derived format,
     /// and will automatically update whenever this object is changed.
-    pub fn create<D: FormatDescriptor>(&self, descriptor: D, derived_formats: &[Derived<D::Format>]) -> Handle<<D::Format as Format>::Kind> {
+    pub fn allocate<F: Format>(&self, descriptor: AllocationDescriptor<F>) -> Data<F::Kind> {
         todo!()
     }
 
@@ -122,19 +195,19 @@ impl FrostContext {
         todo!()
     }
 
-    /// Schedules a command to execute on format data.
-    pub fn schedule(&self, descriptor: CommandDescriptor<impl Send + Sync + FnOnce(CommandContext)>) {
+    /// Schedules the provided command buffer for execution.
+    pub fn submit(&self, buffer: CommandBuffer) {
         todo!()
     }
 }
 
-impl std::fmt::Debug for FrostContext {
+impl std::fmt::Debug for DataFrostContext {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         todo!()
     }
 }
 
-impl WorkProvider for FrostContext {
+impl WorkProvider for DataFrostContext {
     fn change_notifier(&self) -> &ChangeNotifier {
         todo!()
     }
@@ -147,17 +220,16 @@ impl WorkProvider for FrostContext {
 /// Marks a derived format that should be accessible and kept
 /// automatically up-to-date when the parent object changes.
 pub struct Derived<F: Format> {
-    holder: PhantomData<&'static F>
+    inner: Arc<()>,
+    marker: PhantomData<F>
 }
 
 impl<F: Format> Derived<F> {
-    /// Specifies that a derived `T` should be created to track an object of type `F`.
-    pub fn of<T: DerivedFormat<F>>() -> Self {
+    /// Specifies how a derived object should be created to track an object of type `F`.
+    pub fn new<D: DerivedDescriptor<F>>(descriptor: D) -> Self {
         todo!()
     }
 }
-
-impl<F: Format> Copy for Derived<F> {}
 
 impl<F: Format> Clone for Derived<F> {
     fn clone(&self) -> Self {
@@ -165,51 +237,15 @@ impl<F: Format> Clone for Derived<F> {
     }
 }
 
-impl<F: Format> std::fmt::Debug for Derived<F> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
-    }
-}
-
-/// References an object of a specific [`Kind`]. The object is backed
-/// by a single primary format and some number of derived formats.
-pub struct Handle<K: Kind> {
-    marker: PhantomData<K>
-}
-
-impl<K: Kind> Handle<K> {
-    /// Creates a view of this data of the given format and mutability.
-    /// The format must either be the primary format or one of the derived
-    /// formats specified during object creation. If this is a mutable view,
-    /// then the format must be the primary format. Using views with invalid
-    /// mutability or formats will lead to panics when [`FormatContext::schedule`]
-    /// is called.
-    pub fn view<M: Mutability, F: Format>(&self) -> View<M, F> {
-        todo!("Where to put the check for getting data?")
-    }
-}
-
-impl<K: Kind> Clone for Handle<K> {
-    fn clone(&self) -> Self {
-        todo!()
-    }
-}
-
-impl<K: Kind> std::fmt::Debug for Handle<K> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
-    }
-}
-
-/// References a specified format underlying a [`Handle`].
+/// References a specified format underlying a [`Data`] instance.
 pub struct View<M: Mutability, F: Format> {
-    inner: Handle<F::Kind>,
+    inner: Data<F::Kind>,
     marker: PhantomData<&'static (M, F)>
 }
 
 impl<M: Mutability, F: Format> View<M, F> {
-    /// Gets the handle to which this view refers.
-    pub fn handle(&self) -> &Handle<F::Kind> {
+    /// Gets the data to which this view refers.
+    pub fn data(&self) -> &Data<F::Kind> {
         &self.inner
     }
 }
@@ -273,6 +309,10 @@ mod tests {
 
     impl Format for FlatArray3d {
         type Kind = Image3d;
+
+        fn allocate(descriptor: &<Self::Kind as Kind>::FormatDescriptor) -> Self {
+            Self
+        }
     }
 
     pub struct IntersectionBuffer;
@@ -285,53 +325,43 @@ mod tests {
 
     impl Format for IntersectionBuffer {
         type Kind = Image3d;
-    }
 
-    impl DerivedFormat<FlatArray3d> for IntersectionBuffer {
-        fn create(descriptor: &Image3dDescriptor, parent: &FlatArray3d) -> Self {
+        fn allocate(descriptor: &<Self::Kind as Kind>::FormatDescriptor) -> Self {
             Self
         }
-
-        fn update(&mut self, parent: &FlatArray3d, usages: &[<Self::Kind as Kind>::UsageDescriptor]) {
-            println!("Update ib");
-        }
     }
 
-    pub struct FlatArray3dDescriptor {
+    pub struct IntersectionBufferFromFlatArray3d;
 
-    }
+    impl DerivedDescriptor<FlatArray3d> for IntersectionBufferFromFlatArray3d {
+        type Format = IntersectionBuffer;
 
-    impl FormatDescriptor for FlatArray3dDescriptor {
-        type Format = FlatArray3d;
-
-        fn create(self) -> Self::Format {
-            FlatArray3d
-        }
-
-        fn descriptor(&self) -> <<Self::Format as Format>::Kind as Kind>::FormatDescriptor {
-            Image3dDescriptor {
-                depth: 29
-            }
-        }
-
-        fn label(&self) -> Option<&'static str> {
-            None
+        fn update(&self, data: &mut Self::Format, parent: &FlatArray3d, usages: &[<<FlatArray3d as Format>::Kind as Kind>::UsageDescriptor]) {
+            todo!()
         }
     }
 
     #[test]
     fn test_api() {
-        let ctx = FrostContext::new();
-        let handle = ctx.create(FlatArray3dDescriptor {}, &[Derived::of::<IntersectionBuffer>()]);
-        let view = handle.view::<Const, IntersectionBuffer>();
+        let ctx = DataFrostContext::new(ContextDescriptor { label: Some("my context") });
+        let data = ctx.allocate::<FlatArray3d>(AllocationDescriptor {
+            descriptor: Image3dDescriptor { depth: 29 },
+            label: Some("my data"),
+            derived_formats: &[&Derived::new(IntersectionBufferFromFlatArray3d)]
+        });
+        
+        let mut command_buffer = CommandBuffer::new(CommandBufferDescriptor { label: Some("my command buffer") });
+        let view = data.view::<Const, IntersectionBuffer>();
 
-        ctx.schedule(CommandDescriptor {
+        command_buffer.schedule(CommandDescriptor {
             label: Some("Test command"),
             command: |ctx| {
                 println!("Execute.");
             },
             views: &[&view]
         });
+
+        ctx.submit(command_buffer);
 
         let intersection_buffer = ctx.get(&view);
         intersection_buffer.test_it();
